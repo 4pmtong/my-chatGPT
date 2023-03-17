5b6f8c7456c9bbce1d92e1cbd9cee87017575d67
Tong Chen <tong.chen@shopee.com>
2023-03-17 14:15:52 +0800

    :tada: init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-  // tailwind.config.js
-  module.exports = {
-    purge: [],
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
